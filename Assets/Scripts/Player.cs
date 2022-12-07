13518fabe6e72f7c1dce17c1e117d9c06c2ca149
angusfang <chris9966647@gmail.com>
2022-12-08 00:20:05 +0800

    Player can summon dragon and fight

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Networking;
 using UnityEngine.UIElements;
 
@@ -16,7 +17,6 @@ public class Player :NetworkBehaviour
     {
         if(!IsOwner) return;
         MoveToSpawnPositionServerRpc(OwnerClientId);
-        //transform.position = SpawnPositions[OwnerClientId];
     }
 
     private void Update()
@@ -26,22 +26,24 @@ public class Player :NetworkBehaviour
         if (Input.GetKey(KeyCode.G) && createCD <= 0f)
         {
             createCD = createCD_const;
-            SpawnMonsterServerRpc(OwnerClientId);
+            Vector3 spawnPosition = transform.position + transform.forward * MonsterPrefab.GetComponent<NavMeshAgent>().radius;
+            SpawnMonsterServerRpc(OwnerClientId, spawnPosition);
         }
-        if (Input.GetMouseButtonUp(0)) SendRayServerRpc(Camera.main.ScreenPointToRay(Input.mousePosition));
+        if (Input.GetMouseButtonUp(0)) SendRayServerRpc(OwnerClientId, Camera.main.ScreenPointToRay(Input.mousePosition));
         
     }
    
 
     [ServerRpc]
-    void SendRayServerRpc(Ray ray)
+    void SendRayServerRpc(ulong clientID, Ray ray)
     {
-        RaycastHit hitinfo;
-        if (!Physics.Raycast(ray, out hitinfo)) return;
-        if (hitinfo.collider == null) return;
-        GameObject hitGameObject = hitinfo.collider.gameObject;
-        if (!hitGameObject.CompareTag("Monster")) return;
-        Debug.Log(hitGameObject.GetComponent<Monster>().master_id);
+        GameManager.Instance.PlayerRayToSelectState(clientID, ray);
+        //RaycastHit hitinfo;
+        //if (!Physics.Raycast(ray, out hitinfo)) return;
+        //if (hitinfo.collider == null) return;
+        //GameObject hitGameObject = hitinfo.collider.gameObject;
+        //if (!hitGameObject.CompareTag("Monster")) return;
+        //Debug.Log(hitGameObject.GetComponent<Monster>().master_id);
     }
 
     [ServerRpc]
@@ -50,11 +52,13 @@ public class Player :NetworkBehaviour
         NetworkManager.Singleton.ConnectedClients[ClientId].PlayerObject.transform.position = SpawnPositions[OwnerClientId];
     }
     [ServerRpc]
-    void SpawnMonsterServerRpc(ulong ClientId)
+    void SpawnMonsterServerRpc(ulong ClientId, Vector3 spawnPosition)
     {
         GameObject Monster = Instantiate(MonsterPrefab, new Vector3(ClientId, 0f, ClientId), Quaternion.identity);
         Monster.GetComponent<Monster>().master_id = ClientId;
-        Monster.GetComponent<NetworkObject>().Spawn();
+        NetworkObject networkObject = Monster.GetComponent<NetworkObject>();
+        networkObject.transform.position = spawnPosition;
+        networkObject.Spawn();
     }
 
 
